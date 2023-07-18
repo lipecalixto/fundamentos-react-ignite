f482e2bb2839409d87e80f7e5d38c9e31ec3e2c5
lipecalixto <fellipe.calixto.santos@gmail.com>
2023-07-18 17:10:23 -0300

    component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
 
 export function App() {
   return (
     <div>
-      <Header />
-
-      <Post
-        author="Fellipe Calixto"
-        content="Post 1"
-      />
-      <Post
-        author="Nicolle Calixto"
-        content="Post 2"
-      />
+    <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Fellipe Calixto"
+            content="Post 1"
+          />
+          <Post
+            author="Nicolle Calixto"
+            content="Post 2"
+          />
+        </main>
+      </div>
     </div>
   )
 }
