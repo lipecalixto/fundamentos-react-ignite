d5d27443c24d837f9114fcce358af1be7e02949e
lipecalixto <fellipe.calixto.santos@gmail.com>
2023-07-20 13:56:41 -0300

    add funcionalidade de aplaudir

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
@@ -5,8 +7,16 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
@@ -30,9 +40,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
